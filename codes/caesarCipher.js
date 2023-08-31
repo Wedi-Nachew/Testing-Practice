56a86501b4c4ec121bb2978843e41e64306abdb6
Filomn Abrha <filmonabrha2020@gmail.com>
2023-08-31 11:48:47 +0300

    Create a caesarCipher constructor that takes a string and a shift and returns a shifted string

--- codes/caesarCipher.js
+++ codes/caesarCipher.js
@@ -26,5 +26,51 @@ const alphaNumbericConversion = {
     y: 24,
     z: 25,
 };
+const caesarCipher = (text, shift) => {
+    let splitedText = [...text];
+    let upperCases = [];
+    const shiftLetters = () => {
+        splitedText.forEach((item, index) => {
+            if (/[A-Z]/.test(item)) {
+                upperCases.push(index);
+            }
+            if (/[a-zA-Z]/.test(item)) {
+                splitedText[index] =
+                    alphaNumbericConversion[item.toLowerCase()] + shift;
+            }
+        });
+    };
+    const wrapLetters = () => {
+        splitedText.forEach((item, index) => {
+            if (item > 25) {
+                splitedText[index] = item - 26;
+            } else if (item < 0) {
+                splitedText[index] = item + 26;
+            }
+        });
+    };
+    const encryptedText = () => {
+        splitedText.forEach((item, index) => {
+            if (Number.isInteger(item)) {
+                for (const letter in alphaNumbericConversion) {
+                    if (item === alphaNumbericConversion[letter]) {
+                        splitedText[index] = letter;
+                    }
+                }
+            }
+        });
+    };
+    const keepCase = () => {
+        upperCases.forEach((num) => {
+            splitedText[num] = splitedText[num].toUpperCase();
+        });
+    };
 
+    shiftLetters();
+    wrapLetters();
+    encryptedText();
+    keepCase();
+
+    return splitedText.join("");
+};
 module.exports = caesarCipher;
